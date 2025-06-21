80f2f88ee076f8f59f3495e909c49217e23fe64e
mumtajhamita14 <mumtajhamita@gmail.com>
2025-06-22 01:07:50 +0530

    Mumtaj added few lines

---
+++ src/org/gitproject2/GitProject2.java
@@ -0,0 +1,14 @@
+package org.gitproject2;
+
+public class GitProject2 {
+public static void main(String[] args) {
+	
+
+	String s5="Information";
+	int length=s5.length();
+	System.out.println("Length of string s5 :"+length);
+	
+	
+}	
+	
+}
