bed7f76336a64064499985f72e37103d0a8ff0be
mumtajhamita14 <mumtajhamita@gmail.com>
2025-06-25 01:15:43 +0530

    Banu added few lines

--- src/org/gitproject2/GitProject2.java
+++ src/org/gitproject2/GitProject2.java
@@ -1,12 +1,20 @@
 package org.gitproject2;
 
 public class GitProject2 {
-public static void main(String[] args) {
+  public static String name="smart skills";
 	
-
+  	
+	
+	public static void main(String[] args) {
+	
+		 System.out.println(name);
+		 
+		
+	      
 	String s5="Information";
 	int length=s5.length();
 	System.out.println("Length of string s5 :"+length);
+	
 	
 	
 }	
