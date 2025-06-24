39f89e91b955b0b2eac55ca658298d7938a026ac
mumtajhamita14 <mumtajhamita@gmail.com>
2025-06-24 16:41:03 +0530

    Mumtaj add few lines updated

--- src/org/gitproject2/GitProject2.java
+++ src/org/gitproject2/GitProject2.java
@@ -1,12 +1,20 @@
 package org.gitproject2;
 
 public class GitProject2 {
-public static void main(String[] args) {
 	
-
-	String s5="Information";
-	int length=s5.length();
-	System.out.println("Length of string s5 :"+length);
+	private void m1() {
+		System.out.println("Method----1");
+	}
+	
+	
+    public static void main(String[] args) {
+	
+       GitProject2 s = new GitProject2();
+       s.m1();
+	
+       String s5="Information";
+	   int length=s5.length();
+	   System.out.println("Length of string s5 :"+length);
 	
 	
 }	
